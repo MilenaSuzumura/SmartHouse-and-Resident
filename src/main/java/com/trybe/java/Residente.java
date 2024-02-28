1f98bfa9d7a21919a62754159b03c720b8179dfc
MilenaSuzumura <milena.suzumura11@gmail.com>
2024-02-28 19:38:57 -0300

    adicionei as variaveis e criei o constructor

--- src/main/java/com/trybe/java/Residente.java
+++ src/main/java/com/trybe/java/Residente.java
@@ -4,5 +4,15 @@ package com.trybe.java;
  * Classe.
  **/
 public class Residente {
+  String nome;
+  int idade;
+  double peso;
+  double altura;
 
+  public Residente(String nome, int idade, double peso, double altura) {
+    this.nome = nome;
+    this.idade = idade;
+    this.peso = peso;
+    this.altura = altura;
+  };
 }
